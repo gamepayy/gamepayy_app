908e1dfa524242ae3fd7028aef66bde501b184e1
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-07-25 14:58:50 +0300

    testing

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -6,9 +6,7 @@ const BlogPage = () => {
       <div className="flex items-center justify-between">
         <h2 className="font-semibold">Blog</h2>
       </div>
-      <div className="flex items-center justify-center">
-        Under Construction!
-      </div>
+      <div className="flex items-center justify-center">Under Construction</div>
     </div>
   );
 };
